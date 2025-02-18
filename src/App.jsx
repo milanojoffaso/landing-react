75f842804079a375f949af46ec67ecbf192b9dc1
milanojoffaso <melitjoffy@gmail.com>
2025-02-18 14:53:52 +0530

    add header

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 
-import './App.css'
+
 import Header from './components/dashboard/header';
 
 function App() {
--- src/components/dashboard/Header.jsx
+++ src/components/dashboard/Header.jsx
@@ -2,7 +2,25 @@ import React from 'react'
 
 function Header() {
   return (
-    <div>Header</div>
+    <div className="bg-black flex justify-between p-4">
+        <div className="text-white flex items-center">
+         <img class="col-span-2 max-h-12 w-full object-contain lg:col-span-1" src="Tailwind CSS.png" alt="Tailwind" width="158" height="48"/>
+        <span><span className="text-xl font-semibold">Tailwindcss</span></span>
+        </div>
+        <div className="text-white flex items-center">
+            <ul className="flex items-center ">
+                <li className="mr-4"><a href="#">Docs</a></li>
+                <li className="mr-4"><a href="#">components</a></li> 
+                <li className="mr-4"><a href="#">blog</a></li>
+                <li className="mr-4"><a href="#">github</a></li>
+               
+            </ul>
+        </div>
+        
+        <button  class="bg-sky-500 hover:bg-sky-700 ... px-6 py-2 rounded-lg">sign up</button>
+    
+    </div>
+
   )
 }
 
